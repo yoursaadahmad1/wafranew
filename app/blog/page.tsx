286378a5768180852bae58847aa50850c2d8e857
yoursaadahmad1 <mosesnuggz@gmail.com>
2025-06-30 08:47:41 +0300

    updated

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,16 @@
-'use client';
+// 'use client';
 
-import { BlogHero } from '@/components/blog/blog-hero';
-import { BlogList } from '@/components/blog/blog-list';
-import { useGSAP } from '@/hooks/use-gsap';
+// import { BlogHero } from '@/components/blog/blog-hero';
+// import { BlogList } from '@/components/blog/blog-list';
+// import { useGSAP } from '@/hooks/use-gsap';
 
-export default function Blog() {
-  useGSAP();
+// export default function Blog() {
+//   useGSAP();
   
-  return (
-    <div className="overflow-x-hidden">
-      <BlogHero />
-      <BlogList />
-    </div>
-  );
-}
+//   return (
+//     <div className="overflow-x-hidden">
+//       <BlogHero />
+//       <BlogList />
+//     </div>
+//   );
+// }
